a32b91dacc430a5da7e9dcbe19b4beea573f047a
Bruno Mackay <bruno@midtowncapital.ca>
2021-02-01 09:48:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+## About Application
+
+This mini application has a migration for the borrowers table, and You need to do the following:
+
+#### what you need to do is you need to fork this repo (don't clone it);
+
+- Need to create a Borrower Model.
+- Need to create a borrower CRUD view.
+- Keep in mind the borrower will be called by its UUID.
+- Every time you create a new record, will store a random 5 numbers in an (array) in the steps field.
+- DOB will be stored and called as a Carbon Date.
+- Once the borrower is created, he should receive a welcome email from the application through (a queue), that he was created.
+- Create a seeder of 20 borrowers to see them as a list of borrowers.
+##
+
+This mini application need to created by following Laravel best practices, by using DRY and SOLID principles.
+
+#
+Good Luck.
---
+++ database/migrations/2020_07_20_160712_create_borrowers_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrowers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('step');
+            $table->json('steps');
+            $table->string('login_id')->nullable();
+            $table->decimal('monthly_sales', 12, 2)->nullable();
+            $table->decimal('monthly_expenses', 12, 2)->nullable();
+            $table->boolean('other_financing')->nullable();
+            $table->decimal('other_financing_amount', 12, 2)->nullable();
+
+            $table->string('legal_business_name')->nullable();
+            $table->string('business_physical_address')->nullable();
+            $table->string('business_physical_city')->nullable();
+            $table->string('business_physical_province', 2)->nullable();
+            $table->string('business_physical_postal')->nullable();
+            $table->string('email')->nullable();
+            $table->date('dob');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrowers');
+    }
+}
